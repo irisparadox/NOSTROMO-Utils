1fdd6b696b7bfd84d01c5449914a86e5ec924fa8
ParadoxSubject <saavedra.gabriel083@gmail.com>
2022-08-15 22:43:23 +0200

    Added mad and fixed netherite rework.

---
+++ src/main/java/net/paradoxsubject/nostromoutils/EMath.java
@@ -0,0 +1,10 @@
+package net.paradoxsubject.nostromoutils;
+
+public class EMath {
+    /**
+     * Method for clamping in Math
+     */
+    public static double clamp(double val, double min, double max){
+        return Math.max(min, Math.min(max, val));
+    }
+}
--- src/main/java/net/paradoxsubject/nostromoutils/mixin/utilsItem/NetheriteToolsRework.java
+++ src/main/java/net/paradoxsubject/nostromoutils/mixin/utilsItem/NetheriteToolsRework.java
@@ -1,11 +1,13 @@
 package net.paradoxsubject.nostromoutils.mixin.utilsItem;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterials;
+import net.paradoxsubject.nostromoutils.EMath;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,18 +15,31 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(PlayerEntity.class)
 public class NetheriteToolsRework {
     /**
-     * Redirects from the method to an instance of the class BlockState fetching a float to the method getEfficiency
-     * from an instance of EnchantmentHelper, fetching an integer.
+     * Modifies a value that is fetched from <code>PlayerEntity.getBlockBreakingSpeed()</code> in the <code>EnchantmentHelper.getEfficiency()</code>
+     * method, when it's called, and returns a clamped value which is 8/5 times the default value when using a Netherite tool.
+     * @see net.minecraft.entity.player.PlayerEntity#getBlockBreakingSpeed(BlockState)
+     * @see net.minecraft.enchantment.EnchantmentHelper#getEfficiency(LivingEntity)
+     *
+     * @param entity Player's entity.
+     *
+     * @return Returns an integer value which is the modified efficiency level.
      * */
     @Redirect(method = "getBlockBreakingSpeed(Lnet/minecraft/block/BlockState;)F",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getEfficiency(Lnet/minecraft/entity/LivingEntity;)I"))
     public int getEfficiency(LivingEntity entity) {
+        //We get the item from the main hand and the efficiency level
         Item mainHandItem = entity.getHandItems().iterator().next().getItem();
         int efficiencyLevel = EnchantmentHelper.getEfficiency(entity);
 
+        //We check if the material of the main hand item is Netherite
         if (mainHandItem instanceof ToolItem && ((ToolItem) mainHandItem).getMaterial().equals(ToolMaterials.NETHERITE))
-            efficiencyLevel = Math.max(efficiencyLevel * 8 / 5, 255);
+            /*
+             * In here I used a function which has a slope of 8/5 so if the input is 5, the output will be 8.
+             * This means we can instamine blocks we couldn't, only using Netherite.
+             * We also clamp the values to 255, so it doesn't break the game's limits.
+             * */
+            efficiencyLevel = (int) EMath.clamp((double) efficiencyLevel * 8 / 5, 0, 255);
 
-        return Math.max(efficiencyLevel, 255);
+        return efficiencyLevel;
     }
 }
